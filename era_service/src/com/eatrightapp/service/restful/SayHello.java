0eb660002e6fda9661843b883a0c63eaab6711cf
nick.smith <nick.smith@gmail.com>
2011-02-23 05:55:15 +0000

    Moved project into trunk.

---
+++ era_service/src/com/eatrightapp/service/restful/SayHello.java
@@ -0,0 +1,15 @@
+package com.eatrightapp.service.restful;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+@Path("/helloworld")
+public class SayHello {
+    @GET
+    @Produces("text/plain")
+    public String getItems() {
+        return "restful web service example";
+    }
+
+}
