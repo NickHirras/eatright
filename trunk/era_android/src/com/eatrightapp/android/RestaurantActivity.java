df1e278f7e378cad206cb6dc347845f750b4a5f8
nick.smith@gmail.com <nick.smith@gmail.com@15bd0a23-f475-c759-4b5c-24a3b3e0b96d>
2011-02-23 04:50:47 +0000

    Tweaking the Restaurant activity.

--- trunk/era_android/src/com/eatrightapp/android/RestaurantActivity.java
+++ trunk/era_android/src/com/eatrightapp/android/RestaurantActivity.java
@@ -3,40 +3,38 @@ package com.eatrightapp.android;
 import java.text.DecimalFormat;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.ActivityNotFoundException;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.util.Linkify;
 import android.view.View;
-import android.widget.ImageButton;
+import android.view.View.OnClickListener;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.eatrightapp.android.lazylist.ImageLoader;
 import com.eatrightapp.external.yelp.v2.BusinessDetail;
-import com.eatrightapp.external.yelp.v2.Coordinate;
 import com.eatrightapp.external.yelp.v2.YelpService;
-import com.eatrightapp.util.Convert;
 
 public class RestaurantActivity extends Activity {
 
 	private EatRightApp app;
 
 	private ImageLoader imageLoader;
-	
-	private DecimalFormat distanceFormat = new DecimalFormat("#,##0.0");
-	
+		
 	private TextView restaurantNameTV;
 	private TextView addressTV;
+	private TextView mapTV;
 	private ImageView ratingImageIV;
 	private TextView reviewsQtyTV;
-	private TextView distanceTV;
 	private ImageView restaurantImageIV;
 	private TextView snippetTextTV;
 	private ImageView snippetImageIV;
-	private ImageButton mapButton;
-	private ImageButton directionsButton;
-	private ImageButton callButton;
-	private ImageButton walkingButton;
+	private TextView phoneTV;
 	
 	/** Called when the activity is first created. */
 	@Override
@@ -51,14 +49,11 @@ public class RestaurantActivity extends Activity {
 		
 		restaurantNameTV = (TextView) findViewById(R.id.Restaurant_RestaurantName);
 		addressTV = (TextView) findViewById(R.id.Restaurant_Address);
+		mapTV = (TextView) findViewById(R.id.Restaurant_Map);
 		ratingImageIV = (ImageView) findViewById(R.id.Restaurant_RatingImage);
 		reviewsQtyTV = (TextView) findViewById(R.id.Restaurant_ReviewsQty);
-		distanceTV = (TextView) findViewById(R.id.Restaurant_Distance);		
 		restaurantImageIV = (ImageView) findViewById(R.id.Restaurant_Image);
-		mapButton = (ImageButton) findViewById(R.id.Restaurant_MapButton);
-		directionsButton = (ImageButton) findViewById(R.id.Restaurant_DrivingDirectionsButton);
-		callButton = (ImageButton) findViewById(R.id.Restaurant_CallButton);
-		walkingButton = (ImageButton) findViewById(R.id.Restaurant_WalkingDirectionsButton);
+		phoneTV = (TextView) findViewById(R.id.Restaurant_Phone);
 		
 		// TODO: This may need to be done in an asynctask with a progress dialog.
 		BusinessDetail biz = YelpService.findBusiness(getIntent().getExtras().getString("com.eatrightapp.android.PlacesSearchActivity.YelpId"));
@@ -67,12 +62,38 @@ public class RestaurantActivity extends Activity {
 		
 		if (biz.getLocation() != null && biz.getLocation().getDisplayAddress() != null) {
 			StringBuilder address = new StringBuilder();
+			final StringBuilder mapAddress = new StringBuilder();
 			for (String addrLine : biz.getLocation().getDisplayAddress()) {
-				address.append(addrLine).append("\n");
+				address.append(addrLine).append("<br />");
+				mapAddress.append(addrLine).append(", ");
 			}
-			addressTV.setText(address.toString());
+			addressTV.setText(Html.fromHtml(address.toString()));
+			mapTV.setText(Html.fromHtml(" (<a href=#>Map</a>) "));
+			mapTV.setClickable(true);
+			mapTV.setOnClickListener(new OnClickListener() {
+
+				@Override
+				public void onClick(View v) {
+					try {
+						Intent intent = new Intent(android.content.Intent.ACTION_VIEW, Uri.parse("geo:0,0?q=" + Uri.encode(mapAddress.toString())));
+						startActivity(intent);
+					} catch(ActivityNotFoundException ex) {
+						AlertDialog.Builder builder = new AlertDialog.Builder(RestaurantActivity.this);
+						builder.setMessage("Unable to launch a map.")
+					       .setCancelable(true)
+					       .setPositiveButton("Okay", new DialogInterface.OnClickListener() {
+					           public void onClick(DialogInterface dialog, int id) {
+					        	   RestaurantActivity.this.finish();
+					           }
+					       });
+						builder.show();
+					}
+				}
+				
+			});
 		} else {
 			addressTV.setText("");
+			mapTV.setText("");
 		}
 
 		if(biz.getRatingImgUrl() != null) {
@@ -80,35 +101,30 @@ public class RestaurantActivity extends Activity {
 			imageLoader.DisplayImage(biz.getRatingImgUrl().toExternalForm(), RestaurantActivity.this, ratingImageIV);			
 		}
 
-		reviewsQtyTV.setText(biz.getReviewCount() + " reviews");
-
-		distanceTV.setText(distanceFormat.format(Convert.metersToMiles(biz.getDistance())) + " mi");
+		reviewsQtyTV.setAutoLinkMask(Linkify.WEB_URLS);
+		reviewsQtyTV.setText(Html.fromHtml("<a href=" + biz.getMobileUrl() + ">" + biz.getReviewCount() + " Yelp user reviews...</a>"));
+		final String bizMobileLink = biz.getMobileUrl().toExternalForm();
+		reviewsQtyTV.setOnClickListener(new OnClickListener() {
+	
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(android.content.Intent.ACTION_VIEW, Uri.parse(bizMobileLink));
+				startActivity(intent);				
+			}
+			
+		});
+		
 		
 		if(biz.getImageUrl() != null) {
 			restaurantImageIV.setTag(biz.getImageUrl().toExternalForm());
 			imageLoader.DisplayImage(biz.getImageUrl().toExternalForm(), RestaurantActivity.this, restaurantImageIV);			
 		}
 		
-		if(biz.getPhone() == null) {
-			callButton.setEnabled(false);
+		if(biz.getDisplayPhone() == null) {
+			phoneTV.setText("");
 		} else {
-			final String phoneNumber = biz.getPhone();
-			callButton.setOnClickListener(new View.OnClickListener() {
-				public void onClick(View v) {
-					Intent dialer = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + phoneNumber));
-					startActivity(dialer);
-				}
-			});
+			phoneTV.setText(biz.getDisplayPhone());
 		}
-		
-		final String bizName = biz.getName();
-		final Coordinate coordinate = biz.getLocation().getCoordinate();
-		mapButton.setOnClickListener(new View.OnClickListener() {
-			public void onClick(View v) {
-				Intent intent = new Intent(android.content.Intent.ACTION_VIEW, 
-					Uri.parse("geo:0,0?q=" + coordinate.getLatitude() + "," + coordinate.getLongitude() + " (" + bizName + ")" ));
-				startActivity(intent);
-			}
-		});
+				
 	}
 }
